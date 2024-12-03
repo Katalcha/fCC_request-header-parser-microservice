1b1cac92128ac867555b23d062053864b2e8e684
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-12-03 16:56:57 +0100

    feat: add /api/whoami
    feat: finish project

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.get('/api/hello', (_, res) => {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  try {
+    res.json({
+      ipaddress: req.ip,
+      language: req.get('accept-language'),
+      software: req.get('user-agent')
+    });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
 // listen for requests 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
